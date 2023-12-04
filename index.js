2523211766fc90991ef2eeca4d673c7e7bc04863
Apiwch <129593511+Apiwch@users.noreply.github.com>
2023-12-04 20:50:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,58 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 var cors = require('cors')
+const fs = require('fs');
+var multer = require('multer');
 var { InfluxDBClient, Point } = require('@influxdata/influxdb3-client');
+const path = require('path');
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(express.static('public'));
 
 app.set('port', (process.env.PORT || 4000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(bodyParser.raw({ type: 'image/*', limit: '10mb' }));
 
 const token = 'z-UccinXjSvvmdbLTEBAYTQp9nErpJVOLajQU7zgqlZgNwJUPaZOAUgYw55RoTa3vPmQ_x9eVFzSmCCxXl6QZA==';
 
 const client = new InfluxDBClient({host: 'https://eu-central-1-1.aws.cloud2.influxdata.com', token: token})
 
+
+
+
+
+
 let data = [];
+
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    const imageName = 'uploaded_image.jpg';
+    fs.writeFileSync(path.join(__dirname, 'public', imageName), req.file.buffer);
+  
+    // Send a response to the client
+    res.send(`<img src="/${imageName}" alt="Uploaded Image"/>`);
+});
+
+app.get('/api/getImage', (req, res) => {
+    const imageName = 'uploaded_image.jpg';
+    const imagePath = path.join(__dirname, 'public', imageName);
+  
+    // Read the image file and send it in the response
+    fs.readFile(imagePath, (err, data) => {
+      if (err) {
+        res.status(500).send('Error reading the image file');
+        return;
+      }
+  
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(data);
+    });
+  });
 
 app.get('/', function(req, res){
     
@@ -26,6 +64,7 @@ app.post('/api/SLM', function (req, res) {
 	let database = `SLM`
     const SL = req.query.SLM;
     const T = req.query.Ti;
+
     const SLRecord = {
         SL: SL,
         timestamp: T
@@ -37,7 +76,7 @@ app.post('/api/SLM', function (req, res) {
     const points =
     [
         Point.measurement("SL")
-            .setFloatField("SLV", SL)
+            .setIntegerField("SLV", SL)
     ];
     for (let i = 0; i < points.length; i++) {
         const point = points[i];
@@ -47,21 +86,23 @@ app.post('/api/SLM', function (req, res) {
     }
     
     data.push(SLRecord);
-	res.send('success : ' + req.query.SLM)
+	res.send('success : ' + req.query.SLM +' '+ req.query.Ti)
 })
 
 app.get('/api/SLM', function (req, res) {
+
     res.json(data);
 });
 
 app.get('/api/His', function (req, res){
     let dateNow = new Date().toISOString().split('T')[0];
     var data = [];
+    
     async function main() {
         const query = `SELECT *
         FROM "SL"
         WHERE
-        time >= timestamp '${dateNow}T03:00:00.000Z' AND time <= timestamp '${dateNow}T15:00:00.000Z'`;
+        time >= timestamp '${dateNow}T01:00:00.000Z' AND time <= timestamp '${dateNow}T13:00:00.000Z'`;
         const rows = await client.query(query, 'SLM');
 
         for await (const row of rows) {
@@ -74,7 +115,6 @@ app.get('/api/His', function (req, res){
             } 
             data.push(SlvHis);
         }
-
         res.json(data);
     }
     
